f26f185fe51bff14df2fb60950a6b6f185cae122
MARCO AURELIO <marcocpdti@gmail.com>
2021-06-25 16:30:59 -0300

    added user interaction via linux terminal

--- deletingLinesEquals.py
+++ deletingLinesEquals.py
@@ -1,11 +1,14 @@
 import pandas as pd
 
-path = '/home/marco/Documentos/errors.csv'
+# path = '/home/marco/Documentos/errors.csv'
+
+path = input('Digite o caminho do arquivo csv: ')
+colName = input('Digite a coluna que será gravada: ')
 
 dataFile = pd.read_csv('/home/marco/Documentos/errors.csv', encoding='utf-8', delimiter=',')
 
-stringPositionOne = dataFile['erro'].str.split(r'\\n').str[0]
-stringPositionTwo = dataFile['erro'].str.split(r'\\n').str[1]
+stringPositionOne = dataFile[colName].str.split(r'\\n').str[0]
+stringPositionTwo = dataFile[colName].str.split(r'\\n').str[1]
 
 dataSpreadsheet = stringPositionOne + ' -> ' + stringPositionTwo
 
